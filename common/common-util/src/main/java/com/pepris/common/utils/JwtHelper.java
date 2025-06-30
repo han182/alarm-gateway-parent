02771bc7a127b885f23a1e94bd882077d3e902bd
hs621 <18230053891@163.com>
2025-06-30 19:52:25 +0800

    实现jwt整合

---
+++ common/common-util/src/main/java/com/pepris/common/utils/JwtHelper.java
@@ -0,0 +1,69 @@
+package com.pepris.common.utils;
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+/**
+ * 生成JSON Web令牌的工具类
+ */
+public class JwtHelper {
+    // token过期时间 365天
+    private static long tokenExpiration = 365 * 24 * 60 * 60 * 1000;
+    // 加密密钥 实际上是生成的唯一的字符串或者是随机的字符串
+    private static String tokenSignKey = "123456";
+
+    //根据用户id和用户名称生成token字符串
+    public static String createToken(Long userId, String username) {
+        String token = Jwts.builder()
+                .setSubject("AUTH-USER")// 分组的组名
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))// 过期时间
+                .claim("userId", userId) // 有效载荷
+                .claim("username", username) // 有效载荷
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey) // 加密
+                .compressWith(CompressionCodecs.GZIP)// 压缩
+                .compact();
+        return token;
+    }
+
+    // 从token中获取userid
+    public static Long getUserId(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return null;
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            Integer userId = (Integer) claims.get("userId");
+            return userId.longValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // 从token中获取username
+    public static String getUsername(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return "";
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            return (String) claims.get("username");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void removeToken(String token) {
+        //jwttoken无需删除，客户端扔掉即可。
+    }
+
+    //测试上面的方法用的
+    public static void main(String[] args) {
+        String token = JwtHelper.createToken(1L, "admin");//"eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJSCjAK0A0Ndg1S0lFKrShQsjI0MzY2sDQ3MTbQUSotTi3yTFGyMjKEsP0Sc1OBWp6unfB0f7NSLQDxzD8_QwAAAA.2eCJdsJXOYaWFmPTJc8gl1YHTRl9DAeEJprKZn4IgJP9Fzo5fLddOQn1Iv2C25qMpwHQkPIGukTQtskWsNrnhQ";//JwtHelper.createToken(7L, "admin");
+        System.out.println(token);
+        System.out.println(JwtHelper.getUserId(token));
+        System.out.println(JwtHelper.getUsername(token));
+    }
+}
